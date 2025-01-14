f10cb40e23d4559ff7aba2e54671e5099ce22427
Victor Garzon <victor.garzon@bain.com>
2025-01-14 14:50:34 -0800

    Add copilot.vim

--- init.lua
+++ init.lua
@@ -950,6 +950,7 @@ require('lazy').setup({
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
+  { 'github/copilot.vim' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
