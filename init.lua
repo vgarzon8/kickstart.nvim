58aa7493e11808f5a0daa106437c3d0c818a987d
Victor Garzon <victor.garzon@bain.com>
2025-01-29 07:51:34 -0800

    Add render markdown

--- init.lua
+++ init.lua
@@ -1048,6 +1048,17 @@ require('lazy').setup({
       words = { enabled = false },
     },
   },
+
+  -- Improve viewing of Markdown files
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
