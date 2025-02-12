6f7776cfdc5325337566c598aa093fe04f0d6301
Victor Garzon <victor.garzon@bain.com>
2025-02-11 21:06:45 -0800

    Add vim tmux navigator

--- init.lua
+++ init.lua
@@ -1059,6 +1059,26 @@ require('lazy').setup({
     ---@type render.md.UserConfig
     opts = {},
   },
+
+  -- Navigate sbetween neovim and tmux panes / splits using consistent key mappings
+  {
+    'christoomey/vim-tmux-navigator',
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+      'TmuxNavigatePrevious',
+      'TmuxNavigatorProcessList',
+    },
+    keys = {
+      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+    },
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
